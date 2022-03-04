658be09dbb3b3087e2d3bfb7aaf1bd2961f9bd1b
Piotr Bujak <piotr.bujak@hotmail.com>
2022-03-04 15:13:21 +0100

    implement game prepare

---
+++ MemoryMotoGame/MemoryGame/GamePrepare.cs
@@ -0,0 +1,15 @@
+﻿class GamePrepare
+{
+    DifficultyConfiguration difficulty = new GameDifficultyFactory().Create(new DifficultyReader().Choice());
+    List<Word> words = new WordLoader().Create();
+    GuessPrepareService GuessPrepareService = new GuessPrepareService();
+
+    public void Execute()
+    {
+        while (true)
+        {
+
+        }
+    }
+}
+
